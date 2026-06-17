274ff12539614fe9362bf0d4421eb03f65047bff
Fenohasina RATEFINJANAHARY <hei.fenohasina.5@gmail>
2026-06-17 16:31:57 +0300

    Update Admin.java

--- demo/src/main/java/com/fenohasinaKantoMinosoa/transport/Admin.java
+++ demo/src/main/java/com/fenohasinaKantoMinosoa/transport/Admin.java
@@ -1,5 +1,21 @@
 package com.fenohasinaKantoMinosoa.transport;
 
-public class Admin {
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Admin {
+    private int id;
+    private String nom;
+    private String prenom;
+    
+    public Voyage creerVoyage(int id, Centre centreDepart, Centre centreArrivee, int distance, LocalDate dateDepart, Classe classe, int prixBillet, TaxiBrousse taxiBrousse, Chauffeur chauffeur){
+        return Voyage(id, centreDepart, centreArrivee, distance, dateDepart, classe, prixBillet, taxiBrousse, chauffeur, new ArrayList<>(), StatutVoyage.AVENIR);
+    }
 }
